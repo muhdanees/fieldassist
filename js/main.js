e33f2be64c5380ccf642e54558d789e0363e1962
Anees <mohd.anees@htdigital.in>
2019-02-23 14:41:35 +0530

    events

--- js/main.js
+++ js/main.js
@@ -21,9 +21,52 @@ $(document).ready(function () {
             .show();
     });
 
+    function rowHTML(data) {
+        return '<div class="row">' +
+            '<div class="col">' + data.name + '</div>' +
+            '<div class="col text-right">' + data.time + '</div>' +
+        '</div>';
+    }
 
+    // events
+    $.get("js/events.json", function(data) {
+        // submissions
+        var submissions = data.submissions;
+        var $eventSubmission = $('.event-submissions') 
+        submissions.forEach(function(submission){
+            var title = '<strong>' + submission.title + '</strong>';
+            var submissionRow = [];
+            submission.events.forEach(function(item, index){
+                submissionRow[index] = rowHTML(item);
+            });
+            var list = '<li>' + title + submissionRow.join('') + '</li>';
+
+            $eventSubmission.append(list);
+        });
+
+        // exams
+        var exams = data.exams;
+        var $eventExam = $('.event-exams') 
+        exams.forEach(function(exam){
+            var title = '<strong>' + exam.title + '</strong>';
+            var examRow = [];
+            exam.subjects.forEach(function(item, index){
+                examRow[index] = rowHTML(item);
+            });
+            var list = '<li>' + title + examRow.join('') + '</li>';
+
+            $eventExam.append(list);
+        });
+    });
+
+    // resources
     $.get('js/resource.json', function(data){
-        console.log(data)
+        var resources = [];
+        data.resource.forEach(function(resource, index) {
+            resources[index] = '<li>' + '<img src="' + resource.image + '"> ' + resource.name + '</li>'
+        });
+
+        $('.list-resource').append(resources.join(''))
     });
 
 });
